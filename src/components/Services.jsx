526160099f45bd5e306fa05c9d0f6697334200a2
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-07-18 15:38:50 -0400

    correccion services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -32,7 +32,7 @@ export const Services = () => {
                           Frontend Development
                         </h3>
                         <p className="text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500] leading-7">
-                          HTML, CSS, JavaScript,
+                          HTML, CSS, JavaScript, React, Redux
                           <br />
                           TypeScript, Bootstrap, Material UI, Chakra UI
                         </p>
